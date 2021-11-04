4e0ad1f7431c01f75b9b508c7ef250874653d32a
Dogtordog <panimarkerik@gmail.com>
2021-11-05 01:04:31 +0200

    Task 3, stored JSON in jsonbin and retrieved it in script.js

---
+++ res/js/script.js
@@ -0,0 +1,9 @@
+$.ajax({
+    url: 'https://api.jsonbin.io/b/61846433dfffdf47a4906333',
+    type: 'GET',
+    dataType: 'json',
+    success: function(json) {
+      console.log("great success!!")
+      //should do something with json here assuming it works
+    }
+  });
