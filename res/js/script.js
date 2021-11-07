1bdb892b455d91f69711f0eeaa86fe8b854c1a8c
Dogtordog <panimarkerik@gmail.com>
2021-11-07 22:38:07 +0200

    task 5

--- res/js/script.js
+++ res/js/script.js
@@ -11,15 +11,13 @@
   });
   
   */
-
- $(function() {
+/*
+  $(function() {
     $.get('https://api.jsonbin.io/b/6188175d820eda3cc81947aa/1', function(json_obj) {
       console.log(json_obj)
     for (obj of json_obj) {
 
       //console.log(obj)
-
-      let posts =document.getElementById("posts_field");
         
       let post =  document.createElement("div");
       post.classList.add("singlebox");
@@ -47,7 +45,7 @@
             <img src="res/images/like.JPG"  class = "like"/>
           </div>*/
 
-
+/*
 
 
       if (picture!=null){
@@ -69,8 +67,66 @@
 
       post.appendChild(postituseSisu)
 
-      posts.appendChild(post)
+      $("#posts_field").append(post)
     }
   })
 });
-  
+*/
+$(function() {
+  $.get("res/js/posts.json",  function(json_obj) {
+      $.each(json_obj, function(key, value) {
+
+        let posts =document.getElementById("posts_field");
+          
+        let post =  document.createElement("div");
+        post.classList.add("singlebox");
+
+        let posttop =  document.createElement("div");
+        posttop.classList.add("posttop");
+        console.log(posttop.classList)
+              
+        let aeg = document.createElement("label");
+        aeg.innerText = this.time;
+        let author = document.createElement("label");
+        author.innerText = this.author;
+
+        posttop.appendChild(aeg);
+        posttop.appendChild(author);
+        post.appendChild(posttop);
+        
+        let picture = this.image;
+        let postituseSisu =  document.createElement("div");
+        postituseSisu.classList.add("postituseSisu");
+        /*
+        <div class = "postituseSisu">
+              <img src="res/images/pilt2.JPG"  class = "postitusePilt"/>
+              <label>man's not hot</label>
+              <img src="res/images/like.JPG"  class = "like"/>
+            </div>
+        */
+
+
+
+        if (picture!=null){
+          let pilt = document.createElement("img");
+          pilt.src = picture;
+          pilt.classList.add("postitusePilt");
+          pilt.src
+          postituseSisu.appendChild(pilt);
+        }
+
+        let sisu = document.createElement("label");
+        sisu.innerText = this.caption;
+        postituseSisu.appendChild(sisu);
+
+        let like = document.createElement("img");
+        like.src = "res/images/like.JPG";
+        like.classList.add("like");
+        postituseSisu.appendChild(like);
+
+        post.appendChild(postituseSisu)
+
+        posts.appendChild(post)
+    });
+  })
+});
